88aa956794d84649234156297a969d0c831a23b3
shubv92 <shubinder92@gmail.com>
2023-08-26 18:42:11 +0100

    NFT: Adding hardhat

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Button from './Button';
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
 require('@nomiclabs/hardhat-waffle');
+
+const privateKey = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
   solidity: '0.8.4',
   networks: {
     localhost: {
       url: 'http://localhost:8545',
+    },
+    hardhat: {
+      chainId: 1337,
     },
   },
 };
